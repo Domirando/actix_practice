7f5a6756d89173deaa487aef76362181b6f51918
Domirando <vohidjonovnamaftuna@gmail.com>
2025-10-30 11:40:52 +0500

    changed

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,37 @@ async fn main() {
         App::new()
             .route("/", get().to(|| async {HttpResponse::Ok().body("get")}))
             .service(hello)
-
     })
 
-.bind("0.0.0.0:3000")
-.unwrap()
-    .run()
-    .await
+    .bind("0.0.0.0:3000")
     .unwrap()
-    //println!("Hello, world!");
+        .run()
+        .await
+        .unwrap()
 }
 
-#[post("/user")]
-async fn user(info: web::Json<Info>) -> impl Responder {
-    let msg = format!("name: {}, age: {}", info.name, info.age);
-    HttpResponse::Ok().body(msg)
-    }
-
-#[derive(Deserialize)]
-struct Info{
-    name: String,
-    age: i32
-    }
-
-#[get("/hello")]
-async fn hello() -> impl Responder {
+#[get("/home")]
+async fn home() -> impl Responder {
     let person = Person{name: "Maftuna".to_string(), age: 20};
     let person_json = serde_json::to_string(&person).unwrap();
     HttpResponse::Ok().json(person_json)
-
-    }
+}
 
 #[derive(Serialize)]
 struct Person {
     name: String,
     age: i32
+}
+
+#[post("/user")]
+async fn user(info: web::Json<User>) -> impl Responder {
+    let msg = format!("name: {}, age: {}", info.name, info.age);
+    HttpResponse::Ok().body(msg)
     }
+
+#[derive(Deserialize)]
+struct User{
+    name: String,
+    age: i32
+}
+
